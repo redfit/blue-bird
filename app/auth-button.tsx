c615ef93c868f657f4c89f78fa15d45e7f163229
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-28 23:06:08 +0900

    Restrict Access to Authenticated Users with Supabase RLS Policies

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function AuthButton() {
   const supabase = createClientComponentClient();
+  const router = useRouter();
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "github",
@@ -12,6 +14,7 @@ export default function AuthButton() {
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
+    router.refresh();
   };
 
   return (
