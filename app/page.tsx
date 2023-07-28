4695d5f17a480575db556bd844958dcbf7472627
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-07-28 21:29:50 +0900

    Authenticate Users with Github OAuth using Supabase and Next.js Client Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import Image from "next/image";
 import { cookies } from "next/headers";
+import AuthButton from "@/app/auth-button";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data: tweets } = await supabase.from("tweets").select("*");
+
   return (
     <>
+      <AuthButton />
       <pre>{JSON.stringify(tweets, null, 2)}</pre>
     </>
   );
