28a31aa7098d31716b0688138fab77a7738db15f
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-08-01 21:13:46 +0900

    Dynamically Update UI with Database Changes using Supabase Realtime

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Likes from "@/app/likes";
-import { experimental_useOptimistic as useOptimistic } from "react";
+import { experimental_useOptimistic as useOptimistic, useEffect } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
   const [optimisticTweets, addOptimisticTweets] = useOptimistic<
@@ -14,6 +16,29 @@ export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
     newOptimisticTweets[index] = newTweet;
     return newOptimisticTweets;
   });
+
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime tweets")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "tweets",
+        },
+        (payload) => {
+          router.refresh();
+        },
+      )
+      .subscribe();
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
 
   return optimisticTweets.map((tweet) => (
     <div key={tweet.id}>
